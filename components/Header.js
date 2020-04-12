b403b5a038d6b8b0afce81ba2bb4f61d1668a68f
zondayang <u226699@gmail.com>
2020-04-12 23:17:44 +0800

    Add header.moduels.css

--- components/Header.js
+++ components/Header.js
@@ -1,42 +1,17 @@
 import Link from 'next/link'
 
+import styles from '../assets/components/Header.module.css'
+
 export default function Header(props) {
   return (
-    <header>
+    <header className={styles.header}>
       <Link href="/">
-        <img src="/logo.png" alt="logo" />
+        <img className={styles.headerImg} src="/logo.png" alt="logo" />
       </Link>
-      <nav>
+      <nav className={styles.headerNav}>
         <span>{props.title}</span>
       </nav>
       <a></a>
-      <style jsx global>{`
-        header {
-          display: grid;
-          grid-template-columns: repeat(3, 1fr);
-          align-items: center;
-          padding: 5px 3%;
-          background-color: #141414;
-          box-shadow: 2px 2px 5px black;
-        }
-      `}</style>
-      <style jsx>{`
-        img {
-          height: 8vh;
-          max-height: 40px;
-          cursor: pointer;
-        }
-
-        nav {
-          display: flex;
-          justify-content: center;
-        }
-
-        nav span {
-          font-size: 1.25rem;
-          color: #55F;
-        }
-      `}</style>
     </header>
   )
 }
